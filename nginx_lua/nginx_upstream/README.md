bf61fae439d1f0ff2fd1eb1a95ed64328cc147d0
shih <shih@knownsec.com>
2016-10-19 14:14:06 +0800

    增加balancer阶段retrie测试

---
+++ nginx_lua/nginx_upstream/README.md
@@ -0,0 +1,4 @@
+# 测试使用方式
+> sudo docker-compose up
+> curl "http://localhost:5555/"
+> cat log/error.log
---
+++ nginx_lua/nginx_upstream/config/lua/balancer_phase.lua
@@ -0,0 +1,19 @@
+local balancer = require "ngx.balancer"
+
+local alist = ngx.ctx.access_list
+for i=1, #alist, 1 do
+    local data = table.remove(alist, 1)
+    if #data == 2 and type(data) == 'table' then
+        ngx.ctx.access_list = alist
+        balancer.set_current_peer(data[1], data[2])
+        if #alist >= 1 then
+            ok, err = balancer.set_more_tries(1)
+            if not ok then 
+                print("set more tries error:", err)
+            end
+        end
+        return
+    else
+        print("unknown data:", data)
+    end
+end
