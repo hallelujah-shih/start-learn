ebee04c0cba64453f4d80c73bdb05f648e382da8
sh19871122 <sh19871122@gmail.com>
2016-10-30 23:11:18 +0800

    增加debug版本的nginx

--- nginx_lua/nginx_upstream/README.md
+++ nginx_lua/nginx_upstream/README.md
@@ -4,3 +4,14 @@
 > curl "http://localhost:5555/"
 > cat log/error.log
 ```
+
+## debug nginx 
+```
+ps aux|grep nginx
+找出work process
+
+cd /usr/src/openresty-xxxx
+gdb -p pid
+
+* 用tmux分割窗口来操作，不然没法请求
+```
--- nginx_lua/nginx_upstream/config/lua/balancer_phase.lua
+++ nginx_lua/nginx_upstream/config/lua/balancer_phase.lua
@@ -1,20 +1,18 @@
 local balancer = require "ngx.balancer"
 
+print('into balancer')
 local alist = ngx.ctx.access_list
 ngx.var.srcip = #alist
-for i=1, #alist, 1 do
-    local data = table.remove(alist, 1)
-    if #data == 2 and type(data) == 'table' then
-        ngx.ctx.access_list = alist
-        balancer.set_current_peer(data[1], data[2])
-        if #alist >= 1 then
-            ok, err = balancer.set_more_tries(1)
-            if not ok then 
-                print("set more tries error:", err)
-            end
+
+local data = table.remove(alist, 1)
+if #data == 2 and type(data) == 'table' then
+    balancer.set_current_peer(data[1], data[2])
+    if #alist >= 1 then
+        local ok, err = balancer.set_more_tries(1)
+        if not ok then 
+            print("set more tries error:", err)
         end
-        return
-    else
-        print("unknown data:", data)
     end
+else
+    print("unknown data:", data)
 end
