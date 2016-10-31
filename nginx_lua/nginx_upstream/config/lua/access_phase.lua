1423e3894517778fc477b314d9c5d74358856fde
shih <shih@knownsec.com>
2016-10-31 21:04:06 +0800

    调整nginx_upstream的镜像生成，使得镜像中能够直接使用ipython requests gdb等

--- nginx_lua/nginx_upstream/config/lua/access_phase.lua
+++ nginx_lua/nginx_upstream/config/lua/access_phase.lua
@@ -1,3 +1,3 @@
-ngx.ctx.access_list = {{'122.114.45.153', 9090}, {'122.114.38.225', 9999}, {'122.114.38.225', 80}}
+ngx.ctx.access_list = {{'122.114.45.153', 80}}
 ngx.var.srcip = "hello"
 print("into access_phase srcip:", ngx.var.srcip)
--- nginx_lua/nginx_upstream/config/lua/balancer_phase.lua
+++ nginx_lua/nginx_upstream/config/lua/balancer_phase.lua
@@ -4,15 +4,16 @@ print('into balancer')
 local alist = ngx.ctx.access_list
 ngx.var.srcip = #alist
 
+if #alist == 0 then
+    print("-------------------------------------------------")
+    return ngx.exit(500)
+end
+
 local data = table.remove(alist, 1)
-if #data == 2 and type(data) == 'table' then
-    balancer.set_current_peer(data[1], data[2])
-    if #alist >= 1 then
-        local ok, err = balancer.set_more_tries(1)
-        if not ok then 
-            print("set more tries error:", err)
-        end
+balancer.set_current_peer(data[1], data[2])
+if #alist >= 1 then
+    local ok, err = balancer.set_more_tries(1)
+    if not ok then 
+        print("set more tries error:", err)
     end
-else
-    print("unknown data:", data)
 end
